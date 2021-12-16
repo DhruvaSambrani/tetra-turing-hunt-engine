57cc02f00863041ca585a6e8965d210245057d01
Dhruva Sambrani <44899822+DhruvaSambrani@users.noreply.github.com>
2021-12-16 15:32:36 +0530

    Follow naming conventions and split Map functions to Map.py

--- src/terminal.py
+++ src/terminal.py
@@ -1,19 +1,14 @@
+from map import Map
 from textual.app import App
 from textual.widgets import ScrollView
 import numpy as np
 
-def get_block(text, rows, columns, startrow, startcolumn):
-    return "\n".join([
-        i[startcolumn:startcolumn+columns]
-        for i in text.split("\n")[startrow : startrow+rows]
-    ])
-
 class SimpleApp(App):
     async def on_mount(self) -> None:
-        self.map = open("test").read()
+        self.map = Map("test")
         self.position = np.array([0,0])
-        self.size = np.array([10, 20])
-        self.body = ScrollView(get_block(self.map, *self.size, *self.position))
+        self.size = np.array([25, 50])
+        self.body = ScrollView(self.map.get_block(*self.size, *self.position))
         await self.view.dock(self.body)
 
     async def on_load(self):
@@ -25,7 +20,7 @@ class SimpleApp(App):
     
     async def action_move(self, direction):
         self.position += np.array(direction)
-        await self.body.update(get_block(self.map, *self.size, *self.position))
+        await self.body.update(self.map.get_block(*self.size, *self.position))
 
-SimpleApp.run(title="Test Map", log="textual.log")
+SimpleApp.run(title="Test Map")
 
