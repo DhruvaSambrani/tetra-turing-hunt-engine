838e2981c187905009e7f7dfad35a78f95be3bce
Akshay <sakshays.2000@gmail.com>
2022-03-18 17:22:25 +0530

    programmatically convert json dict to settings class

--- src/lib/settings.py
+++ src/lib/settings.py
@@ -4,18 +4,9 @@ import json
 class Settings():
     def __init__(self, settingsjson):
         jsonobj = json.loads(open(settingsjson, encoding="utf-8").read())
-        self.player = jsonobj["player"]
-        self.viewport = jsonobj["viewport"]
-        self.itemspath = jsonobj["itemspath"]
-        self.mapspath = jsonobj["mapspath"]
-        self.surfacespath = jsonobj["surfacespath"]
+        for k, v in jsonobj.items():
+            setattr(self, k, v)
         
-        self.transition_time = jsonobj["transition_time"]
-        self.rest_energy = jsonobj["rest_energy"]
-        self.walking_energy_cost = jsonobj["walking_energy_cost"]
-        self.gadget_classes = jsonobj["gadget_classes"]
-
-
     def itemfile(self, it):
         return self.itemspath + "/" + it
 
