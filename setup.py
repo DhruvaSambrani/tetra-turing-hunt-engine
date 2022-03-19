9780f9a3ed44342398d5b1cebad781f7f84d372a
Dhruva Sambrani <44899822+DhruvaSambrani@users.noreply.github.com>
2022-03-19 17:24:11 +0530

    add setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name='tetra',
+      version='0.1',
+      description='The turing hunt game engine',
+      url='http://github.com/dhruvasambrani/turing-hunt-engine',
+      author='Dhruva Sambrani, Akshay Shankar',
+      license='GNU-AGPL',
+      packages=['tetra'],
+      zip_safe=False)
